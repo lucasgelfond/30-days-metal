72aaafe7c0e5972f895bb6d4a56bf8ea7dadc644
lucas gelfond <lucasgelfond@gmail.com>
2026-02-08 13:12:44 -0500

    day 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,3 @@
+import Metal
+let device = MTLCreateSystemDefaultDevice()!
+print("Device name: \(device.name)")
---
+++ thirtydays/thirtydays/thirtydaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  thirtydaysApp.swift
+//  thirtydays
+//
+//  Created by Lucas Gelfond on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct thirtydaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
