874b29f5f20d5b41b1900fe59e739d6b975e0bc5
lucas gelfond <lucasgelfond@gmail.com>
2026-02-08 13:16:52 -0500

    day 2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import Metal
 let device = MTLCreateSystemDefaultDevice()!
 print("Device name: \(device.name)")
+
+let buffer = device.makeBuffer(length: 16, options: [])!
+print("Buffer is \(buffer.length) bytes in length")
+let points = buffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
+points[0] = SIMD2<Float>(6, 6)
+points[1] = SIMD2<Float>(7,7)
+let p0 = points[0]
+let p1 = points[1]
+print("p0: \(p0), p1: \(p1)")
