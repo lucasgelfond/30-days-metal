d5042d3fc25392483aebd37f5505494f34e1ae10
lucas gelfond <lucasgelfond@gmail.com>
2026-02-08 13:28:48 -0500

    day 3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,11 +2,28 @@ import Metal
 let device = MTLCreateSystemDefaultDevice()!
 print("Device name: \(device.name)")
 
-let buffer = device.makeBuffer(length: 16, options: [])!
-print("Buffer is \(buffer.length) bytes in length")
-let points = buffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
+let commandQueue = device.makeCommandQueue()!
+let commandBuffer = commandQueue.makeCommandBuffer()!
+commandBuffer.addCompletedHandler { completedCommandBuffer in
+    print("Command buffer completed")
+}
+
+
+let sourceBuffer = device.makeBuffer(length: 16, options: [])!
+let destBuffer = device.makeBuffer(length: 16, options: [])!
+let points = sourceBuffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
 points[0] = SIMD2<Float>(6, 6)
 points[1] = SIMD2<Float>(7,7)
 let p0 = points[0]
 let p1 = points[1]
 print("p0: \(p0), p1: \(p1)")
+let blitCommandEncoder = commandBuffer.makeBlitCommandEncoder()!
+blitCommandEncoder.copy(from: sourceBuffer, sourceOffset: 0, to: destBuffer, destinationOffset: 0, size: MemoryLayout<SIMD2<Float>>.stride * 2)
+blitCommandEncoder.endEncoding()
+commandBuffer.addCompletedHandler { completedCommandBuffer in
+    let outPoints = destBuffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
+    let p1 = outPoints[1]
+    print("p1 in destination buffer is \(p1)")
+}
+commandBuffer.commit()
+
